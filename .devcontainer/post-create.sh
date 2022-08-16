20f9a65ae7dd04e7f033d7085616c27807d7633b
exilvm <exilvm@disroot.org>
2022-08-17 00:38:42 +0100

    switch to npm

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,8 +2,5 @@
 
 sudo apt update && sudo apt -y upgrade
 
-yarn global add @devcontainers/cli
+npm i  @devcontainers/cli ts-node -G
 brew install neovim
-
-# Install Node packages
-yarn global add ts-node
