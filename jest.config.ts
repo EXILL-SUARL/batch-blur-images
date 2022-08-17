608f888fe46cec35e34541ed05fb2da0030d1f2c
3x!1 <exilvm@disroot.org>
2022-08-17 21:21:58 +0100

    fix Jest's incompatibility with ESM

--- jest.config.ts
+++ jest.config.ts
@@ -2,16 +2,17 @@ import { Config } from '@jest/types'
 
 // TODO: (in progress) exclude build so the npm test does not fail. UPDATE: Jest ignores tests in the build but TS still compiles them.
 const config: Config.InitialOptions = {
-  preset: `ts-jest/presets/default-esm`,
+  preset: 'ts-jest/presets/default-esm',
   globals: {
     'ts-jest': {
       useESM: true,
     },
   },
-  modulePathIgnorePatterns: ["<rootDir>/build/"],
+  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': `$1`,
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
 }
 
 export default config
