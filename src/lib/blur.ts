0b46b3d1ecf26e0e5cdfe20f8f977a29a4f33490
3x!1 <exilvm@disroot.org>
2022-08-17 21:23:38 +0100

    turn the entry into a promise to simplify testing

--- src/lib/blur.ts
+++ src/lib/blur.ts
@@ -16,27 +16,43 @@ const getFileList = async (dirName: string) => {
   return files
 }
 
-type sigma = number | boolean
+const entry = (targetDir: string, sigma: number | boolean) => {
+  return new Promise(function (resolve, reject) {
+    getFileList(targetDir).then((files) => {
+      try {
+        files.forEach(async (file) => {
+          // TODO: implement a dynamic list of supported formats by Sharp once sharp@v0.31.0 gets released: https://github.com/lovell/sharp/issues/2642#issuecomment-1180197850
+          const supportedFormats = [
+            'jpg',
+            'jpeg',
+            'gif',
+            'webp',
+            'tif',
+            'png',
+            'jpeg',
+          ]
+          const fileExt = path.extname(file).substring(1)
+          if (isImage(file) && supportedFormats.includes(fileExt)) {
+            try {
+              await sharp(file)
+                .blur(sigma)
+                .toBuffer()
+                .then((buffer) => {
+                  sharp(buffer).toFile(file)
+                })
+            } catch (err) {
+              throw err
+              return
+            }
+          }
+        })
 
-const blurImage = async (file: string, sigma: sigma) => {
-  // TODO: implement a dynamic list of supported formats by Sharp once sharp@v0.31.0 gets released: https://github.com/lovell/sharp/issues/2642#issuecomment-1180197850
-  const supportedFormats = ['jpg', 'jpeg', 'gif', 'webp', 'tif', 'png', 'jpeg']
-  const fileExt = path.extname(file).substring(1)
-  if (isImage(file) && supportedFormats.includes(fileExt)) {
-    await sharp(file)
-      .blur(sigma)
-      .toBuffer()
-      .then((buffer) => {
-        sharp(buffer).toFile(file)
-      })
-      .catch((err) => {
-        throw err
-      })
-  }
-}
-
-export default (targetDir: string, sigma: sigma) => {
-  getFileList(targetDir).then((files) => {
-    files.forEach(async (file) => blurImage(file, sigma))
+        resolve(files.length)
+      } catch (err) {
+        reject(err)
+      }
+    })
   })
 }
+
+export default entry
