735e3d33b661d1a909ad3d68501d6c28986c68c0
3x!1 <exilvm@disroot.org>
2022-08-17 20:46:30 +0100

    update todo

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 import { Config } from '@jest/types'
 
-// TODO: exclude build so the npm test does not fail
+// TODO: (in progress) exclude build so the npm test does not fail. UPDATE: Jest ignores tests in the build but TS still compiles them.
 const config: Config.InitialOptions = {
   preset: `ts-jest/presets/default-esm`,
   globals: {
